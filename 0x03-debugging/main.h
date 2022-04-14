634d025f8f9dc8a9653e7a517e8240b9825d37c9
1991199119991 <tadam687@gmail.com>
2022-04-14 22:47:36 +0300

    task 0 e

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
 #ifndef main.h
 #define main.h
 
+/* main.h */
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -8,4 +10,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* main.h */
+#endif
