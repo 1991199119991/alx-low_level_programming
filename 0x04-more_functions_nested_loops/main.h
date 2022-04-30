e1616b8cab2a88b811ad82e46d24c7c8e63d8286
1991199119991 <tadam687@gmail.com>
2022-04-30 06:19:43 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,3 @@
-#ifndef main_h
-#define main_h
-
-/**
- * File: main.h
- * Auth: adam tilahun
- */
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -18,5 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
